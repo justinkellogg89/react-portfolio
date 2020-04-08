fa5abf1d80cfd63bfe6e93d430417ff88ac69172
Justin <jlkellogg18@outlook.com>
2020-04-08 16:30:12 -0700

    portfolio route

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./pages/about";
-import Contact from "./pages/contact";
+import About from "./pages/about/about";
+import Contact from "./pages/contact/contact";
+import Portfolio from "./pages/portfolio/portfolio";
 import Wrapper from "./components/Wrapper";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Navbar /> */}
+        <Nav />
         <Wrapper>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
         </Wrapper>
       </div>
     </Router>
